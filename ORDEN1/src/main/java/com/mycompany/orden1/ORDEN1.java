cda0380a6eaad2e457195b52dd6a1c391d4def4c
YUCHIANG1 <yuchiancao@gmail.com>
2023-08-17 19:43:10 -0500

    yuchiang

---
+++ ORDEN1/src/main/java/com/mycompany/orden1/ORDEN1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.orden1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Alumno
+ */
+public class ORDEN1 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] numeros = new int[5];
+        
+    }
+}
