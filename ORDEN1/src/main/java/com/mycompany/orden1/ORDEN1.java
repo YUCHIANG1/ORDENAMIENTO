363b8a295aef39b9e080fea44646f92c969c5d09
YUCHIANG1 <yuchiancao@gmail.com>
2023-08-17 19:45:58 -0500

    ingresando los numeros

--- ORDEN1/src/main/java/com/mycompany/orden1/ORDEN1.java
+++ ORDEN1/src/main/java/com/mycompany/orden1/ORDEN1.java
@@ -16,5 +16,22 @@ public class ORDEN1 {
         Scanner scanner = new Scanner(System.in);
         int[] numeros = new int[5];
         
+        // Ingreso de los 5 numeros
+        System.out.print("Primer numero: ");
+        numeros[0] = scanner.nextInt();
+        
+        System.out.print("Segundo numero: ");
+        numeros[1] = scanner.nextInt();
+
+        System.out.print("Tercer numero: ");
+        numeros[2] = scanner.nextInt();
+        
+        System.out.print("Cuarto numero: ");
+        numeros[3] = scanner.nextInt();
+        
+        System.out.print("Quinto numero: ");
+        numeros[4] = scanner.nextInt();
+        
+        // Commit  1: Ingreso de los 5 numeros
     }
 }
